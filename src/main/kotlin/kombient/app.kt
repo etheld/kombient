71e150e97ff1a53c98b2f404d0c681b9b3e2cc1b
Peter Varsanyi <Peter.Varsanyi@img.com>
2018-01-12 14:08:34 -0800

    enable logging for all requests

--- src/main/kotlin/kombient/app.kt
+++ src/main/kotlin/kombient/app.kt
@@ -2,10 +2,25 @@ package kombient
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.filter.CommonsRequestLoggingFilter
+
+
 
 
 @SpringBootApplication(scanBasePackages = ["kombient", "me.ramswaroop.jbot"])
-open class Application
+open class Application {
+    @Bean
+    open fun logFilter(): CommonsRequestLoggingFilter {
+        val filter = CommonsRequestLoggingFilter()
+        filter.setIncludeQueryString(true)
+        filter.setIncludePayload(true)
+        filter.setMaxPayloadLength(10000)
+        filter.isIncludeHeaders = false
+        filter.setAfterMessagePrefix("REQUEST DATA : ")
+        return filter
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
