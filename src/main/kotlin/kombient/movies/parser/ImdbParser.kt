e32a7ee7ccbfc48023872d29f5c9a0515bf8c3fb
Peter Varsanyi <pevarsanyi@expedia.com>
2018-07-26 00:39:50 -0700

    skip on empty string

--- src/main/kotlin/kombient/movies/parser/ImdbParser.kt
+++ src/main/kotlin/kombient/movies/parser/ImdbParser.kt
@@ -93,7 +93,7 @@ class ImdbParser(
                 .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36")
                 .maxBodySize(MAX_BODY_SIZE_30M)
                 .get()
-        } while (nextLink != "#")
+        } while (nextLink != "#" || nextLink == "")
         return ratings
     }
 
